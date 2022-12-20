247bafbfc047f3aa260868d794930b9827a3279c
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-12-21 01:51:49 +0200

    Fixed CORS

--- internal/service/middleware/positionMiddleware.go
+++ internal/service/middleware/positionMiddleware.go
@@ -14,7 +14,52 @@ func CheckManagerPosition() func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			accessLevel := r.Context().Value("accessLevel").(staffRes.AccessLevel)
 			if accessLevel < staffRes.Manager {
-				helpers.Log(r).Info("insufficient user permissions")
+				helpers.Log(r).Info("insufficient user permissions (Manager)")
+				ape.RenderErr(w, problems.Forbidden())
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func CheckAdminPosition() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			accessLevel := r.Context().Value("accessLevel").(staffRes.AccessLevel)
+			if accessLevel < staffRes.Admin {
+				helpers.Log(r).Info("insufficient user permissions (Administrator)")
+				ape.RenderErr(w, problems.Forbidden())
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func CheckAccountantPosition() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			accessLevel := r.Context().Value("accessLevel").(staffRes.AccessLevel)
+			if accessLevel < staffRes.Accountant {
+				helpers.Log(r).Info("insufficient user permissions (Accountant)")
+				ape.RenderErr(w, problems.Forbidden())
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func CheckWorkerPosition() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			accessLevel := r.Context().Value("accessLevel").(staffRes.AccessLevel)
+			if accessLevel < staffRes.Worker {
+				helpers.Log(r).Info("insufficient user permissions (Worker)")
 				ape.RenderErr(w, problems.Forbidden())
 				return
 			}
